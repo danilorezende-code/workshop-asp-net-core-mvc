2e176e2049ef4073f522e1b1233904aebc5faebe
danilorezende-code <danilorezende@gmail.com>
2020-05-23 21:37:39 -0300

    SQL Server adaptation and first migration

---
+++ SalesWebMVC/Models/Department.cs
@@ -0,0 +1,18 @@
+﻿namespace SalesWebMVC.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
